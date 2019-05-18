cf3f635be4773e4349ae05ee2cbb7014928efd81
stav <stav@email.com>
2019-05-18 13:13:23 +0300

    trying to make hessian hessian invertable

--- logistic_regression.py
+++ logistic_regression.py
@@ -6,10 +6,12 @@ from numpy.linalg import inv
 import mnist_handler
 from matplotlib import pyplot
 
+
 def sigmoid(x):
     # print('sig')
     # print(x)
     return 1/(1+exp(-x))
+
 
 def vector_sigmoid(X):
     n = (np.shape(X))[0]
@@ -48,6 +50,27 @@ def logistic_objective(X, Y, w):
     return -(1/m)*(c1_log_sigXw + c2_log_sigXw)
 
 
+def regularized_logistic_objective(X, Y, w, lamda=0.01):
+    m = (np.shape(Y)[0])
+    n = (np.shape(X)[0])
+    Xtranspose = np.transpose(X)
+
+    # for debugging purposes
+    zzz = np.matmul(Xtranspose, w)
+
+    sigXw = vector_sigmoid(np.matmul(Xtranspose, w))
+    c1 = (Y > 0)
+    c2 = (c1 == False)
+    # veclog = np.vectorize(log)
+
+    c1_log_sigXw = np.matmul(c1, vector_log(sigXw))
+    c2_log_sigXw = np.matmul(c2, vector_log(1-sigXw))
+
+    norm = np.linalg.norm(w)
+
+    return -(1/m)*(c1_log_sigXw + c2_log_sigXw) + lamda * norm
+
+
 def logistic_gradient(X, Y, w):
     m = (np.shape(Y)[0])
     # n = (np.shape(X)[0])
@@ -57,7 +80,20 @@ def logistic_gradient(X, Y, w):
 
     sigXw = vector_sigmoid(np.matmul(np.transpose(X), w))
 
-    return (1/m)*np.matmul(X,sigXw-c1)
+    return (1/m)*np.matmul(X, sigXw-c1)
+
+
+def regularized_logistic_gradient(X, Y, w, lamda=0.01):
+    m = (np.shape(Y)[0])
+    # n = (np.shape(X)[0])
+    c1 = (Y > 0)
+    # c2 = (c1 == False)
+    # sig = np.vectorize(sigmoid)
+
+    sigXw = vector_sigmoid(np.matmul(np.transpose(X), w))
+
+    return (1/m)*np.matmul(X, sigXw-c1) + lamda * w
+
 
 def logistic_hessian(X, Y, w):
     n = (np.shape(X)[0])
@@ -67,6 +103,16 @@ def logistic_hessian(X, Y, w):
     D = np.diag(sigXw * (np.ones(m)-sigXw))
 
     return (1/m)*np.matmul(np.matmul(X, D),Xtranspose)
+
+
+def regularized_logistic_hessian(X, Y, w, lamda = 0.01):
+    n = (np.shape(X)[0])
+    m = (np.shape(Y)[0])
+    Xtranspose = np.transpose(X)
+    sigXw = vector_sigmoid(np.matmul(Xtranspose, w))
+    D = np.diag(sigXw * (np.ones(m)-sigXw))
+
+    return (1/m)*np.matmul(np.matmul(X, D),Xtranspose) + lamda
 
 
 # finds a stepsize alpha using the Armijo method
@@ -105,8 +151,6 @@ def newton_line_search(A, b, x, grad, dn, fx, alpha0 = 0.00007, beta = 0.5, c = 
     return alphaj
 
 
-
-
 def mnist_GD(A, b, x=None, iterations=100, tolerance=0.1, criteria=0):
 
     if x is None:
@@ -126,8 +170,6 @@ def mnist_GD(A, b, x=None, iterations=100, tolerance=0.1, criteria=0):
     return x, plot1, plot2
 
 
-
-
 def mnist_newton(A, b, x=None, iterations=100, tolerance=0.1, criteria=0):
 
     if x is None:
@@ -137,10 +179,35 @@ def mnist_newton(A, b, x=None, iterations=100, tolerance=0.1, criteria=0):
     plot1 = []
     plot2 = []
 
+    epsilon = 10**(-5)
+
     for iter in range(iterations):
         fx = logistic_objective(A, b, x)
         grad = logistic_gradient(A, b, x)
         hess = logistic_hessian(A, b, x)
+        # inv_hess = inv(logistic_hessian(A, b, x))
+        inv_hess = inv(np.diag(np.diag(hess)) + epsilon * np.eye(n))
+        dn = np.matmul(inv_hess, grad)
+        alpha = newton_line_search(A, b, x, grad, dn, fx)
+        x = x - alpha * dn
+        plot1.append(fx)
+
+    return x, plot1, plot2
+
+
+def regularized_mnist_newton(A, b, x=None, iterations=1000, lamda=1, tolerance=0.1, criteria=0):
+
+    if x is None:
+        n = (A.shape)[0]
+        x = np.zeros(n)
+
+    plot1 = []
+    plot2 = []
+
+    for iter in range(iterations):
+        fx = regularized_logistic_objective(A, b, x, lamda)
+        grad = regularized_logistic_gradient(A, b, x, lamda)
+        hess = regularized_logistic_hessian(A, b, x, lamda)
         # inv_hess = inv(logistic_hessian(A, b, x))
         inv_hess = inv(np.diag(np.diag(hess)))
         dn = np.matmul(inv_hess, grad)
@@ -151,156 +218,222 @@ def mnist_newton(A, b, x=None, iterations=100, tolerance=0.1, criteria=0):
     return x, plot1, plot2
 
 
-
-
-
-
 if __name__ == "__main__":
+    # steepest descent for digits 0 1
+    if True:
+        1
+        # if True:
+        #     Y = mnist_handler.read_label_file(
+        #         'D:\\programs\\pycharm\projects\\numarical_optimization2\\mnist\\train-labels.idx1-ubyte')
+        #     X = mnist_handler.read_image_file(
+        #         'D:\\programs\\pycharm\projects\\numarical_optimization2\\mnist\\train-images.idx3-ubyte')
+        #
+        #     X01, Y01 = mnist_handler.get_digits([0, 1], X, Y)
+        #
+        #     # transforming the matrices representing the images into vectors of pixels
+        #     X01.shape = (np.shape(X01)[0], np.shape(X01)[1] * np.shape(X01)[2])
+        #
+        #     # make matrix of order n x m instead of m x n
+        #     X01 = np.transpose(X01)
+        #
+        #     Y01 = Y01 > 0
+        #
+        #     # w = 0.000002 * np.ones(np.shape(X89)[0])
+        #
+        #     x, plot1, plot2 = mnist_GD(X01, Y01)
+        #
+        #     pyplot.plot(plot1)
+        #     pyplot.show()
+        #
+        #     pred = np.transpose(X01)
+        #
+        #     ans = np.matmul(pred, x)
+        #
+        #     ans = ans>0.5
+        #
+        #     diff = ans != Y01
+        #
+        #     print(sum(diff))
 
-    # Y = mnist_handler.read_label_file(
-    #     'D:\\programs\\pycharm\projects\\numarical_optimization2\\mnist\\train-labels.idx1-ubyte')
-    # X = mnist_handler.read_image_file(
-    #     'D:\\programs\\pycharm\projects\\numarical_optimization2\\mnist\\train-images.idx3-ubyte')
-    #
-    # X01, Y01 = mnist_handler.get_digits([0, 1], X, Y)
-    #
-    # # transforming the matrices representing the images into vectors of pixels
-    # X01.shape = (np.shape(X01)[0], np.shape(X01)[1] * np.shape(X01)[2])
-    #
-    # # make matrix of order n x m instead of m x n
-    # X01 = np.transpose(X01)
-    #
-    # Y01 = Y01 > 0
-    #
-    # # w = 0.000002 * np.ones(np.shape(X89)[0])
-    #
-    # x, plot1, plot2 = mnist_GD(X01, Y01)
-    #
-    # pyplot.plot(plot1)
-    # pyplot.show()
-    #
-    # pred = np.transpose(X01)
-    #
-    # ans = np.matmul(pred, x)
-    #
-    # ans = ans>0.5
-    #
-    # diff = ans != Y01
-    #
-    # print(sum(diff))
-    #
-    #
-    #
+    # newton for digits 8 9
+    if True:
+        1
+        if True:
+            Y = mnist_handler.read_label_file(
+                'D:\\programs\\pycharm\projects\\numarical_optimization2\\mnist\\t10k-labels.idx1-ubyte')
+            X = mnist_handler.read_image_file(
+                'D:\\programs\\pycharm\projects\\numarical_optimization2\\mnist\\t10k-images.idx3-ubyte')
 
-    Y = mnist_handler.read_label_file(
-        'D:\\programs\\pycharm\projects\\numarical_optimization2\\mnist\\t10k-labels.idx1-ubyte')
-    X = mnist_handler.read_image_file(
-        'D:\\programs\\pycharm\projects\\numarical_optimization2\\mnist\\t10k-images.idx3-ubyte')
+            X89, Y89 = mnist_handler.get_digits([8, 9], X, Y)
 
-    X89, Y89 = mnist_handler.get_digits([8, 9], X, Y)
+            # transforming the matrices representing the images into vectors of pixels
+            X89.shape = (np.shape(X89)[0], np.shape(X89)[1] * np.shape(X89)[2])
 
-    # transforming the matrices representing the images into vectors of pixels
-    X89.shape = (np.shape(X89)[0], np.shape(X89)[1] * np.shape(X89)[2])
+            # make matrix of order n x m instead of m x n
+            X89 = np.transpose(X89)
 
-    # make matrix of order n x m instead of m x n
-    X89 = np.transpose(X89)
+            Y89 = Y89 > 8
 
-    Y89 = Y89 > 8
+            # w = 0.000002 * np.ones(np.shape(X89)[0])
 
-    # w = 0.000002 * np.ones(np.shape(X89)[0])
+            x, plot1, plot2 = mnist_newton(X89, Y89)
 
-    x, plot1, plot2 = mnist_newton(X89, Y89)
+            pyplot.plot(plot1)
+            pyplot.show()
 
-    pyplot.plot(plot1)
-    pyplot.show()
+            pred = np.transpose(X89)
 
-    # pred = np.transpose(X89)
-    #
-    # ans = np.matmul(pred, x)
-    #
-    # ans = ans>0.5
-    #
-    # diff = ans != Y89
-    #
-    # print(sum(diff))
+            ans = np.matmul(pred, x)
 
-    # X = np.array([[-1,1,1,4,8],\
-    #               [1,-2,3,6,2],\
-    #               [1,9,5,-7,0],\
-    #               [2,-5,8,3,2],\
-    #               [-43,5,6,-3,5],\
-    #               ])
-    # Y = np.array([0,0,1,0,1,0,0,0])
-    # w = np.array([0, 0, 1, 0, 1, 0, 0, 0])
+            ans = ans>0.5
 
+            diff = ans != Y89
 
-    # X = np.random.rand(5,8)
-    # w = np.random.rand(5)
-    # negate = np.random.rand(5)
-    # negate = negate < 0.5
-    # negate = (-1 * negate) + (negate == False)
-    # w = w * negate
-    # # Y = vector_sigmoid(np.matmul(np.transpose(X), w))
-    #
-    # Y = np.random.rand(8)
-    #
-    # Y = Y > 0.5
-    # #
-    # #
-    # # logistic_gradient(X, Y, w)
-    # # logistic_hessian(X, Y, w)
-    # # logistic_objective(X, Y, w)
-    # #
-    # d = np.random.rand(5)
-    # epsilon = 0.2
-    # f = lambda w: logistic_objective(X,Y,w)
-    # gradf = lambda w: logistic_gradient(X,Y,w)
-    # hessf = lambda w: logistic_hessian(X, Y, w)
-    # # print(f(w+epsilon*d))
-    # # print(f(w))
-    #
-    # last1 = 1
-    # last2 = 1
-    #
-    # for i in range(10):
-    #     epsilon = (0.5)**i
-    #
-    #     curr1 = abs(f(w+epsilon*d)-f(w))
-    #     ratio1 = curr1/last1
-    #
-    #     curr2 = abs(f(w + epsilon * d) - f(w) - epsilon * np.matmul(d, gradf(w)))
-    #     ratio2 = curr2 / last2
-    #
-    #     last1 = curr1
-    #     last2 = curr2
-    #
-    #     print(ratio1)
-    #     # print((abs(f(w+epsilon*d)-f(w)))**2)
-    #     print(ratio2)
-    #     print('\n')
-    #
-    #
-    # last1 = 1
-    # last2 = 1
-    #
-    # for i in range(10):
-    #     epsilon = (0.5) ** i
-    #
-    #     curr1 = norm(gradf(w + epsilon * d) - gradf(w))
-    #     ratio1 = curr1/last1
-    #
-    #     curr2 = norm(gradf(w + epsilon * d) - gradf(w) - np.matmul(hessf(w), epsilon * d))
-    #     ratio2 = curr2 / last2
-    #
-    #     last1 = curr1
-    #     last2 = curr2
-    #
-    #     print(ratio1)
-    #     # print((abs(f(w+epsilon*d)-f(w)))**2)
-    #     print(ratio2)
-    #
-    #     # print((norm(gradf(w + epsilon * d) - gradf(w))) ** 2)
-    #
-    #     print('\n')
-    #
-    #
+            print(sum(diff))
+
+    # gradient and hessian tests
+    if True:
+        1
+        # if True:
+        #     X = np.random.rand(5,8)
+        #     w = np.random.rand(5)
+        #     negate = np.random.rand(5)
+        #     negate = negate < 0.5
+        #     negate = (-1 * negate) + (negate == False)
+        #     w = w * negate
+        #     # Y = vector_sigmoid(np.matmul(np.transpose(X), w))
+        #
+        #     Y = np.random.rand(8)
+        #
+        #     Y = Y > 0.5
+        #     #
+        #     #
+        #     # logistic_gradient(X, Y, w)
+        #     # logistic_hessian(X, Y, w)
+        #     # logistic_objective(X, Y, w)
+        #     #
+        #     d = np.random.rand(5)
+        #     epsilon = 0.2
+        #     f = lambda w: logistic_objective(X,Y,w)
+        #     gradf = lambda w: logistic_gradient(X,Y,w)
+        #     hessf = lambda w: logistic_hessian(X, Y, w)
+        #     # print(f(w+epsilon*d))
+        #     # print(f(w))
+        #
+        #     last1 = 1
+        #     last2 = 1
+        #
+        #     for i in range(10):
+        #         epsilon = (0.5)**i
+        #
+        #         curr1 = abs(f(w+epsilon*d)-f(w))
+        #         ratio1 = curr1/last1
+        #
+        #         curr2 = abs(f(w + epsilon * d) - f(w) - epsilon * np.matmul(d, gradf(w)))
+        #         ratio2 = curr2 / last2
+        #
+        #         last1 = curr1
+        #         last2 = curr2
+        #
+        #         print(ratio1)
+        #         # print((abs(f(w+epsilon*d)-f(w)))**2)
+        #         print(ratio2)
+        #         print('\n')
+        #
+        #
+        #     last1 = 1
+        #     last2 = 1
+        #
+        #     for i in range(10):
+        #         epsilon = (0.5) ** i
+        #
+        #         curr1 = norm(gradf(w + epsilon * d) - gradf(w))
+        #         ratio1 = curr1/last1
+        #
+        #         curr2 = norm(gradf(w + epsilon * d) - gradf(w) - np.matmul(hessf(w), epsilon * d))
+        #         ratio2 = curr2 / last2
+        #
+        #         last1 = curr1
+        #         last2 = curr2
+        #
+        #         print(ratio1)
+        #         # print((abs(f(w+epsilon*d)-f(w)))**2)
+        #         print(ratio2)
+        #
+        #         # print((norm(gradf(w + epsilon * d) - gradf(w))) ** 2)
+        #
+        #         print('\n')
+
+    # gradient and hessian tests for regularized logistic regression
+    if True:
+        1
+        # if True:
+        #     X = np.random.rand(5,8)
+        #     w = np.random.rand(5)
+        #     negate = np.random.rand(5)
+        #     negate = negate < 0.5
+        #     negate = (-1 * negate) + (negate == False)
+        #     w = w * negate
+        #     # Y = vector_sigmoid(np.matmul(np.transpose(X), w))
+        #
+        #     Y = np.random.rand(8)
+        #
+        #     Y = Y > 0.5
+        #     #
+        #     #
+        #     # logistic_gradient(X, Y, w)
+        #     # logistic_hessian(X, Y, w)
+        #     # logistic_objective(X, Y, w)
+        #     #
+        #     d = np.random.rand(5)
+        #     epsilon = 0.2
+        #     lamda = 0.01
+        #     f = lambda w: regularized_logistic_objective(X,Y,w,lamda)
+        #     gradf = lambda w: regularized_logistic_gradient(X,Y,w,lamda)
+        #     hessf = lambda w: regularized_logistic_hessian(X, Y, w,lamda)
+        #     # print(f(w+epsilon*d))
+        #     # print(f(w))
+        #
+        #     last1 = 1
+        #     last2 = 1
+        #
+        #     for i in range(10):
+        #         epsilon = (0.5)**i
+        #
+        #         curr1 = abs(f(w+epsilon*d)-f(w))
+        #         ratio1 = curr1/last1
+        #
+        #         curr2 = abs(f(w + epsilon * d) - f(w) - epsilon * np.matmul(d, gradf(w)))
+        #         ratio2 = curr2 / last2
+        #
+        #         last1 = curr1
+        #         last2 = curr2
+        #
+        #         print(ratio1)
+        #         # print((abs(f(w+epsilon*d)-f(w)))**2)
+        #         print(ratio2)
+        #         print('\n')
+        #
+        #
+        #     last1 = 1
+        #     last2 = 1
+        #
+        #     for i in range(10):
+        #         epsilon = (0.5) ** i
+        #
+        #         curr1 = norm(gradf(w + epsilon * d) - gradf(w))
+        #         ratio1 = curr1/last1
+        #
+        #         curr2 = norm(gradf(w + epsilon * d) - gradf(w) - np.matmul(hessf(w), epsilon * d))
+        #         ratio2 = curr2 / last2
+        #
+        #         last1 = curr1
+        #         last2 = curr2
+        #
+        #         print(ratio1)
+        #         # print((abs(f(w+epsilon*d)-f(w)))**2)
+        #         print(ratio2)
+        #
+        #         # print((norm(gradf(w + epsilon * d) - gradf(w))) ** 2)
+        #
+        #         print('\n')

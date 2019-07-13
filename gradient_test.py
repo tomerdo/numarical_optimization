bda125264a651b6c8fd5f003a64cdf5c667a444a
stav <stav@email.com>
2019-07-13 19:53:18 +0300

    refactoring of gradient tests

--- gradient_test.py
+++ gradient_test.py
@@ -1,6 +1,6 @@
 import numpy as np
 import stochastic_gradient_descent as sgd
-import gradients as grad
+import gradients as grads
 
 
 # an envelope function used in gradient testing
@@ -28,20 +28,20 @@ def grad(X, W, C, v, param, column=0):
         W1 += W
         W1[:, column] = v
         X1 = X
+        return grads.softmax_gradient(X1, W1, C)
     elif param == 'x':
         X1 = np.zeros(X.shape)
         X1 += X
         X1[:, column] = v
         W1 = W
+        return grads.softmax_data_gradient(X1, W1, C)
     else:
         return None
-
-    return sgd.softmax_objective(X1, W1, C)
 
 
 def generate_random_matrices(data_dim, num_of_samples, num_of_classes):
     X = np.random.rand(data_dim, num_of_samples)
-    W = np.random.rand(data_dim, num_of_samples)
+    W = np.random.rand(data_dim, num_of_classes)
     c = np.random.randint(0, num_of_classes, num_of_samples)
 
     C = []
@@ -52,10 +52,10 @@ def generate_random_matrices(data_dim, num_of_samples, num_of_classes):
     return X, W, C
 
 
-def test_ratios(f, gradf, x, iterations=10, epsilon=1, column=0):
+def test_ratios(f, gradf, x, iterations=10, init_epsilon=1, column=0):
     d = np.random.rand(x.shape[0])
-
     curr = f(x)
+    epsilon = init_epsilon
 
     for i in range(iterations):
         last = curr
@@ -63,8 +63,9 @@ def test_ratios(f, gradf, x, iterations=10, epsilon=1, column=0):
         curr = abs(f(x + epsilon * d) - f(x))
         print('ratio 1 is: ', curr / last)
 
-
+    d = np.random.rand(x.shape[0])
     curr = f(x)
+    epsilon = init_epsilon
 
     for i in range(iterations):
         last = curr
@@ -85,36 +86,16 @@ def gradient_test_x(data_dim, num_of_samples, num_of_classes, iterations=10, eps
 
     test_ratios(f, gradf, x, iterations, epsilon, column)
 
-def gradient_test_by_w():
-    global W, C, X
-    A = np.asarray([[1, 1, 4], [1, 1, 1]])
-    W = np.asarray([[0, 0, 1, 0, 0, 0], [0, 0, 0, 0, 0, 1]])
-    C = np.asarray([[0, 0, 0], [0, 0, 0], [1, 0, 0], [0, 0, 1], [0, 0, 0], [0, 1, 0]])
-    print(sgd.softmax_objective(A, W, C))
-    # Gradient testing (w)
-    print("gradient test by w")
-    X = np.random.rand(3, 5)
-    W = np.random.rand(3, 3)
-    c = np.random.randint(0, 3, 5)
-    C = np.asarray([c == 0, c == 1, c == 2])
-    w = W[:, 0]
-    d = np.random.rand(3)
-    epsilon = 1
-    f = lambda w: func(X, W, C, w, 'w', column)
-    gradf = lambda w: grad(X, W, C, w, 'w', column)
-    curr = f(w)
-    last = f(w)
-    for i in range(10):
-        last = curr
-        epsilon = epsilon * 0.5
-        curr = abs(f(w + epsilon * d) - f(w))
-        print('ratio 1 is: ', curr / last)
-    d = np.random.rand(3)
-    epsilon = 0.2
-    curr = f(w)
-    last = f(w)
-    for i in range(10):
-        last = curr
-        epsilon = epsilon * 0.5
-        curr = abs(f(w + epsilon * d) - f(w) - epsilon * sum(d * (gradf(w)[:, 0])))
-        print('ratio 2 is: ', curr / last)
+
+def gradient_test_w(data_dim, num_of_samples, num_of_classes, iterations=10, epsilon=1, column=0):
+    print("gradient testing by w")
+
+    X, W, C = generate_random_matrices(data_dim, num_of_samples, num_of_classes)
+
+    w = W[:, column]
+
+    f = lambda w: func(X, W, C, w, 'w')
+    gradf = lambda w: grad(X, W, C, w, 'w')
+
+    test_ratios(f, gradf, w, iterations, epsilon, column)
+

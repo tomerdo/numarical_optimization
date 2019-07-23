fixing the gradients , running first time the nn from scratch. for now the loss function not decreasing

gradients.py
@@ -11,6 +11,10 @@ def pump(vec, m, n, transpose=True):
 
 # computes the softmax gradient for all weight vectors (W) together
 def softmax_gradient(X, W, C):
+    # adding bias row to the softmax data
+    bias_row = np.ones(X.shape[1])
+    X = np.vstack([X, bias_row])
+
     m = X.shape[1]
     l = W.shape[1]
 
@@ -68,30 +72,28 @@ def JacV_b(relu_derivative, V):
     m = relu_derivative.shape[1]
 
     # iteration over l_i to compute the jacobian
-    relu_mul_v = np.zeros(k)
-    for i in range(k):
-        relu_mul_v[i] = np.matmul(relu_derivative[i], V[i])
-    return relu_mul_v / m
+    # relu_mul_v = np.zeros(k)
+    RV = np.multiply(relu_derivative, V)
+    return RV.sum(axis=1) / m
+
 
 
 def JacV_w(X, relu_derivative, V):
-    k = relu_derivative.shape[0]
     m = relu_derivative.shape[1]
 
-    v_x = np.matmul(V, X.transpose())
-
-    relu_mul_v_x = np.zeros(k)
-    for i in range(k):
-        relu_mul_v_x = np.matmul(relu_derivative[i], v_x[i])
-    return relu_mul_v_x / m
+    # computing relu derivative element wise multiply by V
+    r_v = np.multiply(relu_derivative, V)
+    # computing the result of this by all the samples and return the mean
+    r_v_xt = np.matmul(r_v, X.transpose())
+    return r_v_xt / m
 
 
 def JacV_x(W, relu_derivative, V):
-    k = relu_derivative.shape[0]
+    m = V.shape[1]
+    # computing relu derivative element wise multiply by V
+    r_v = np.multiply(relu_derivative, V)
 
-    relu_mul_v = np.zeros(k)
-    for i in range(k):
-        relu_mul_v[i] = np.matmul(relu_derivative[i], V[i])
+    WRV = np.matmul(W.transpose(), r_v)
+    return WRV
 
-    return np.matmul(np.transpose(W), relu_mul_v)
 